486f0b022f4f6462587cd94b95b83910d70f7e49
Fernando Witt <ferawitt@gmail.com>
2019-10-05 18:48:10 +0200

    Adding entry point for SAK

---
+++ sak.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+__author__ = "Fernando Witt"
+__credits__ = ["Fernando Witt"]
+
+__license__ = "MIT"
+__version__ = "0.0.0"
+__maintainer__ = "Fernando Witt"
+__email__ = "ferawitt@gmail.com"
+
+import os
+import sys
+
+def run():
+    print('Hello world! Sak.py')
