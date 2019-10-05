f0b8c39c1eb40e18b05253bfec29dc4e78f3f85a
Fernando Witt <ferawitt@gmail.com>
2019-10-05 19:48:53 +0200

    Creating miniconda instalation scheme

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
-# sak
-Group everyday tools in a swiss-army-knife command
+# SAK
+
+                         _____  ___   _   __
+                        /  ___|/ _ \ | | / /
+                        \ `--./ /_\ \| |/ / 
+                         `--. \  _  ||    \ 
+                        /\__/ / | | || |\  \
+                        \____/\_| |_/\_| \_/
+
+                                                .:^
+                         ^                     /   :
+            '`.        /;/                    /    /
+            \  \      /;/                    /    /
+             \\ \    /;/                    /  ///
+              \\ \  /;/                    /  ///
+               \  \/_/____________________/    /
+                `/                         \  /
+                {  o              o  }'
+                 \_________________________/
+
+
+Group everyday developer's tools in a swiss-army-knife command.
+
+# Warning
+
+This is a work in progrees.
+
+# Motivation
+
+TODO:
+
+# Instalation
+
+TODO:
+
--- sak.py
+++ sak.py
@@ -11,6 +11,50 @@ __email__ = "ferawitt@gmail.com"
 
 import os
 import sys
+import subprocess
+
+# TODO: This must be the leader pid, so if it dies it will kill all the subprocesses
+
+CURRENT_DIR = os.path.abspath(os.path.dirname(__file__))
+SAK_GLOBAL = os.path.abspath(os.path.join(os.environ.get('HOME'), '.sak'))
+
+# SAK will not use the system python, but will download miniconda
+SAK_PYTHON = os.path.join(SAK_GLOBAL, 'python')
+SAK_PYTHON_BIN = os.path.join(SAK_PYTHON, 'miniconda3', 'bin', 'python3')
+
+def check_python():
+    return os.path.exists(SAK_PYTHON_BIN)
+
+def pip_install():
+    pass
+
+def install_python(ask_confirm=True):
+    if check_python():
+        return
+
+    if ask_confirm:
+        if not raw_input("No python found, would like to install? [Y/N]") in ['Y', 'y', 'yes']:
+            return
+
+    if not os.path.exists(SAK_PYTHON):
+        os.makedirs(SAK_PYTHON)
+
+    miniconda_installer = os.path.join(SAK_PYTHON, 'miniconda.sh')
+    if not os.path.exists(miniconda_installer):
+        subprocess.check_call([
+            'wget',
+            'https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh',
+            '-O',
+            miniconda_installer])
+
+    instalation_prefix = os.path.join(SAK_PYTHON, 'miniconda3')
+    subprocess.check_call(['/usr/bin/env', 'bash', miniconda_installer, '-b', '-p', instalation_prefix])
+
+
+def install():
+    install_python(ask_confirm=False)
+
 
 def run():
-    print('Hello world! Sak.py')
+    install_python()
+
