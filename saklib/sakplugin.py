42641d0fa12c7e13d9642c996a8121abbd443116
Fernando Witt <ferawitt@gmail.com>
2020-02-29 19:09:35 +0100

    Use imp for plugin loading

--- saklib/sakplugin.py
+++ saklib/sakplugin.py
@@ -14,9 +14,7 @@ import os
 
 from pathlib import Path
 import inspect
-import pkgutil
-from importlib import import_module
-
+import imp
 
 class SakPlugin(object):
     def __init__(self, name):
@@ -63,13 +61,21 @@ class SakPluginManager(object):
         if not os.path.exists(pluginsPath):
             return
 
-        for p in os.listdir(pluginsPath):
-            p = os.path.join(pluginsPath, p)
-            if not os.path.isdir(p):
+        for name in os.listdir(pluginsPath):
+            plugin_path = os.path.join(pluginsPath, name)
+            if not os.path.isdir(plugin_path):
                 continue
 
-            for (_, name, _) in pkgutil.iter_modules([Path(pluginsPath)]):
-                imported_module = import_module('.plugin', package=name)
+            for fname in os.listdir(plugin_path):
+
+                fname_abs = os.path.join(plugin_path, fname)
+                if not fname_abs.endswith('.py'):
+                    continue
+                if os.path.isdir(fname_abs):
+                    continue
+
+                # TODO: Make import scheme version independent
+                imported_module = imp.load_source(name, fname_abs)
 
                 for i in dir(imported_module):
                     attribute = getattr(imported_module, i)
@@ -81,6 +87,6 @@ class SakPluginManager(object):
                         continue
 
                     plugin = attribute()
-                    plugin.setPluginPath(p)
+                    plugin.setPluginPath(plugin_path)
                     plugin.setContext(self.context)
                     self.addPlugin(plugin)
