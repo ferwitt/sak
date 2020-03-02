7f4eeb995b0af5608f5096754c1e3bbc22949939
Fernando Witt <ferawitt@gmail.com>
2020-03-02 18:55:28 +0100

    webapp: Show exported commands in the main page

--- plugins/sak-webapp/web/static/js/app.js
+++ plugins/sak-webapp/web/static/js/app.js
@@ -1,3 +1,19 @@
-angular.module("app", []).controller("SakApp", function($scope) {  
+angular.module("app", []).controller("SakApp", function($scope, $http) {  
     $scope.message="Hello World" 
+
+  $scope.plugins = []
+  $http.get("/api/show/plugins")
+  .then(function(response) {
+    $scope.plugins = response.data;
+  });
+
+  $scope.cmd_path = []
+
+  $scope.cmd_root = {}
+  $http.get("/api/cmd/sak")
+  .then(function(response) {
+    $scope.cmd_root = response.data;
+  });
+
+
 } )
